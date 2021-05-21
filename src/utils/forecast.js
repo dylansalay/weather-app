e0b3323f9c1286fc0febde7aab8ea11bde310ed3
dylan salay <dylansalay@gmail.com>
2021-05-21 13:58:32 -0700

    weather message

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -4,6 +4,7 @@ const forecast = (lat, long, callback) => {
   const url = `http://api.weatherstack.com/current?access_key=b714abb8138d4378d4de5a5823ce2585&query=${long},${lat}&units=f`
 
   request({ url, json: true}, (error, { body }) => {
+    console.log(body)
     if(error) {
       callback('Unable to connect to weather service!', undefined)
     } 
@@ -12,7 +13,9 @@ const forecast = (lat, long, callback) => {
     } 
     else {
       let current = body.current
-      callback(undefined, `It is currently ${current.temperature} degrees out and ${current.weather_descriptions[0]}. It feels like ${current.feelslike} degrees out. There is a ${current.precip}% chance of rain`)
+      let weather_desc = current.weather_descriptions.map(desc => `and ${desc.toLowerCase()}`)
+      console.log(weather_desc.join())
+      callback(undefined, `It is currently ${current.temperature} degrees out ${weather_desc.join()}. It feels like ${current.feelslike} degrees out. The humidity level is ${current.humidity}% and there is a ${current.precip}% chance of rain`)
     }
   })
 }
